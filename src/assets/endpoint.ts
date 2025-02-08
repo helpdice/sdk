87382ecb1c9198c4b58319bac937f4400637e540
helpdice <packer@helpdice.com>
2025-02-08 07:43:17 +0530

    Updated Static Path

--- src/assets/endpoint.ts
+++ src/assets/endpoint.ts
@@ -1,7 +1,7 @@
 const assets = {
 	v1: {
-		imagePlaceholder: "image-placeholder.png",
-		avatar: "avatar.png",
+		imagePlaceholder: "images/image-placeholder.png",
+		avatar: "images/avatar.png",
 	},
 };
 
